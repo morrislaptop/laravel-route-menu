dd5d7cdd80fed977f369225af92280bcf1e11326
Craig Morris <craig.michael.morris@gmail.com>
2021-03-23 12:08:54 +0000

    wip

--- src/Commands/LaravelRouteMenuCommand.php
+++ src/Commands/LaravelRouteMenuCommand.php
@@ -3,15 +3,20 @@
 namespace Morrislaptop\LaravelRouteMenu\Commands;
 
 use Closure;
+use Exception;
+use ReflectionMethod;
+use ReflectionFunction;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
 use Illuminate\Routing\Route;
 use Illuminate\Console\Command;
+use ReflectionFunctionAbstract;
 use Illuminate\Support\Collection;
 use Symfony\Component\Console\Input\InputOption;
 use Illuminate\Foundation\Console\RouteListCommand;
-use ReflectionFunction;
-use ReflectionMethod;
+use NunoMaduro\Collision\Highlighter;
+use ReflectionNamedType;
+use ReflectionParameter;
 
 class LaravelRouteMenuCommand extends RouteListCommand
 {
@@ -19,8 +24,12 @@ class LaravelRouteMenuCommand extends RouteListCommand
 
     public $description = 'Your `route:list`, sir.';
 
-    public function handle()
+    protected Highlighter $highlighter;
+
+    public function handle(Highlighter $highlighter = null)
     {
+        $this->highlighter = $highlighter;
+
         $this->router->flushMiddlewareGroups();
 
         if (empty($this->router->getRoutes())) {
@@ -52,68 +61,129 @@ class LaravelRouteMenuCommand extends RouteListCommand
      */
     protected function groupRoutes(Collection $routes)
     {
-        return [
-            [
-                'name' => 'App',
-                'routes' => $routes,
-            ]
-        ];
+        return $routes->groupBy(
+            fn (Route $route) => $this->getNamespaceOrFile($route)
+        );
     }
 
-    protected function displayGroups(array $groups)
+    protected function getNamespaceOrFile(Route $route)
     {
-        foreach ($groups as $group)
+        $reflection = $this->resolveReflection($route);
+
+        if ($reflection instanceof ReflectionMethod) {
+            return $reflection->getDeclaringClass()->getNamespaceName();
+        }
+
+        return str_replace(base_path() . '/', '', $reflection->getFileName());
+    }
+
+    protected function displayGroups(Collection $groups)
+    {
+        foreach ($groups as $namespace => $routes)
         {
-            $this->line($group['name']);
+            $emoji = $this->getEmoji($namespace);
+
+            $this->line("<bg=black;options=bold> $emoji $namespace </>");
 
             $this->line('-----');
 
-            $group['routes']->map(
+            $routes->map(
                 fn (Route $route, $i) => $this->displayRoute($route, $i)
             );
         }
     }
 
+    protected function getEmoji($namespace)
+    {
+        switch ($namespace)
+        {
+            case Str::contains($namespace, 'Fortify'):
+                return '🏰';
+
+            case Str::contains($namespace, 'Jetstream'):
+                return '🛫';
+
+            case Str::contains($namespace, 'Livewire'):
+                return '👀';
+
+            case Str::contains($namespace, 'Spark'):
+                return '⚡';
+
+            case Str::contains($namespace, 'Vapor'):
+                return '☁️';
+
+            default:
+                return '💻';
+        }
+    }
+
     protected function displayRoute(Route $route, int $i)
     {
-        ray($route);
-
         $methods = collect($route->methods())->reject( fn ($method) => $method === 'HEAD');
-        $name = $route->getName() ? " ({$route->getName()})" : '';
+        $padLength = 10;
 
         $this->line(
-            $i
-            . ' - '
-            . implode('|', $methods->all())
+            "<fg=yellow>" . implode('|', $methods->all()) . "</>"
             . ' '
-            . $route->uri()
-            . $name
+            . '/' . ltrim($route->uri(), '/')
         );
 
+        $this->line('');
+
+        if ($route->getName()) {
+            $this->line('🏷️  ' . str_pad("Name: ", $padLength)  . $route->getName());
+        }
+
         if ($route->domain()) {
-            $this->line("Domain: " . $route->domain());
+            $this->line('🌏 ' . str_pad("Domain: ", $padLength) . $route->domain());
         }
 
         if ($route->getActionName()) {
-            $this->line("Action: " . $route->getActionName());
+            $this->line('🎬 ' . str_pad("Action: ", $padLength) . $route->getActionName());
         }
 
-        $this->line("Middleware: " . $this->getMiddleware($route));
+        if ($params = $route->signatureParameters()) {
+            $this->line('🤹 ' . str_pad("Params: ", $padLength) . $this->paramString($params)->implode(', '));
+        }
 
+        if ($middleware = $this->getMiddleware($route)) {
+            $this->line('🖕 ' . str_pad("Middles: ", $padLength) . $middleware);
+        }
+
+        $reflection = $this->resolveReflection($route);
+        $fileName = str_replace(base_path() . '/', '', $reflection->getFileName());
+        $this->line('☕️ ' . str_pad("Code: ", $padLength) . "<fg=green>" . $fileName . '</>:<fg=green>' . $reflection->getStartLine() . '</>');
+
+        $this->line('');
+        $this->line('');
+    }
+
+    /**
+     * @param $params ReflectionParameter[]
+     */
+    protected function paramString($params)
+    {
+        return collect($params)->map(function (ReflectionParameter $param) {
+            $str = $param->allowsNull() ? '?' : '';
+            $str .= "<fg=magenta>" . ($param->getType() instanceof ReflectionNamedType ? $param->getType()->getName() : 'mixed') . "</>";
+            $str .= ' ';
+            $str .= '$' . $param->getName();
+            $str .= $param->isDefaultValueAvailable() ? ' = <fg=green>' . $param->getDefaultValue() . '</>' : '';
+
+            return $str;
+        });
+    }
+
+    protected function resolveReflection(Route $route): ReflectionFunctionAbstract
+    {
         if ($controller = $route->getAction('controller')) {
-            [$class, $method] = explode('@', $controller);
-            $reflection = new ReflectionMethod($class, $method);
+            [$class, $method] = Str::parseCallback($controller, '__invoke');
+            return new ReflectionMethod($class, $method);
         }
         else {
             $closure = $route->getAction('uses');
-            $reflection = new ReflectionFunction($closure);
+            return new ReflectionFunction($closure);
         }
-
-        $fileName = str_replace(base_path() . '/', '', $reflection->getFileName());
-        $this->line("Code: " . $fileName . ':' . $reflection->getStartLine());
-
-        $this->line('');
-        $this->line('');
     }
 
     protected function getMiddleware($route)
@@ -151,6 +221,7 @@ class LaravelRouteMenuCommand extends RouteListCommand
     {
         if (($this->option('name') && ! Str::contains($route->getName(), $this->option('name'))) ||
              $this->option('path') && ! Str::contains($route->uri(), $this->option('path')) ||
+             $this->option('file') && ! Str::contains(strtolower($this->getNamespaceOrFile($route)), strtolower($this->option('file'))) ||
              $this->option('method') && ! Str::contains(implode('|', $route->methods()), strtoupper($this->option('method')))) {
             return;
         }
@@ -169,6 +240,7 @@ class LaravelRouteMenuCommand extends RouteListCommand
             ['method', null, InputOption::VALUE_OPTIONAL, 'Filter the routes by method'],
             ['name', null, InputOption::VALUE_OPTIONAL, 'Filter the routes by name'],
             ['path', null, InputOption::VALUE_OPTIONAL, 'Filter the routes by path'],
+            ['file', null, InputOption::VALUE_OPTIONAL, 'Filter the routes by namespace or file'],
         ];
     }
 }
